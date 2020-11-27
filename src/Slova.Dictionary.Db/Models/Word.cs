e49b6fcae607c800402e60902db12331156241c7
Aleksei Mialkin <anmyalkin@gmail.com>
2020-11-27 21:16:36 +0300

    Adds Required attribute

--- src/Slova.Dictionary.Db/Models/Word.cs
+++ src/Slova.Dictionary.Db/Models/Word.cs
@@ -1,14 +1,21 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Slova.Dictionary.Db.Models
 {
     public class Word
     {
         public int WordId { get; set; }
+        
+        [Required] 
         public string Name { get; set; }
         public string Transcription { get; set; }
+
+        [Required] 
         public string Translation { get; set; }
         public int? Gender { get; set; }
+        
+        [Required] 
         public DateTime CreationDate { get; set; }
 
         public int LanguageId { get; set; }

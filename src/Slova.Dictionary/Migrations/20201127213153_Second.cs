d6d1ed28c7841344f860c158f66c200b4e318ad0
Aleksei Mialkin <anmyalkin@gmail.com>
2020-11-28 00:36:51 +0300

    Adds Second migration

---
+++ src/Slova.Dictionary/Migrations/20201127213153_Second.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Slova.Dictionary.Migrations
+{
+    public partial class Second : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Words",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Words");
+        }
+    }
+}

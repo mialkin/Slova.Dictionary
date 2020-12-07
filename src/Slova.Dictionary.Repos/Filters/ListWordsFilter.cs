82349e66e463f8c9e283b29ae79f39d15feb3bd0
Aleksei Mialkin <anmyalkin@gmail.com>
2020-12-07 18:03:15 +0300

    Adds validation attributes.

--- src/Slova.Dictionary.Repos/Filters/ListWordsFilter.cs
+++ src/Slova.Dictionary.Repos/Filters/ListWordsFilter.cs
@@ -7,9 +7,13 @@ namespace Slova.Dictionary.Repos.Filters
 {
     public class ListWordsFilter
     {
-        [Required] public int UserId { get; set; }
+        [Required]
+        [Range(1, int.MaxValue)]
+        public int UserId { get; set; }
 
-        [Required] public int LanguageId { get; set; }
+        [Required]
+        [Range(1, int.MaxValue)]
+        public int LanguageId { get; set; }
         public int Skip { get; set; }
         public int Take { get; set; } = 100;
 
